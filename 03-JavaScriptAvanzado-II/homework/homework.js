84b345c924b8323b799b2fc282facda9060922a5
David Ruales <hruales1138@gmail.com>
2022-07-05 16:10:06 -0500

    Homework 2

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -6,7 +6,8 @@ function counter() {
   /*
   Ejercicio 1
 
-  La función counter debe retornar otra función. Esta función retornada debe actuar como un contador, retornando un valor numérico que empieza en 1 e incrementa con cada invocación.
+  La función counter debe retornar otra función. Esta función retornada debe actuar como un contador,
+  retornando un valor numérico que empieza en 1 e incrementa con cada invocación.
 
   Ejemplo:
   const nuevoContador = counter()
@@ -21,13 +22,28 @@ function counter() {
    */
 }
 
+const counterFunction = counter();
+console.log(counterFunction())
+console.log(counterFunction())
+console.log(counterFunction())
+console.log(counterFunction())
+console.log(counterFunction())
+
+
 function cacheFunction(cb) {
   /*
   Ejercicio 2
 
-  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
+  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback 
+  que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, 
+  al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que 
+  efectuar otra vez cálculos que ya se hicieron anteriormente.
 
-  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
+  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese 
+  argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto 
+  donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de 
+  manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el 
+  resultado estará guardado en la "memoria caché".
 
 
   Ejemplo:
@@ -38,7 +54,8 @@ function cacheFunction(cb) {
   const squareCache = cacheFunction(square)
 
   squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
-  squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
+  squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de 
+  square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
 
   */
 }
